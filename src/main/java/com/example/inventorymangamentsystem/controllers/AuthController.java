98a0e2a736697736d1bbcb02f07af72d11a53bc7
ShaneEDouglas <112830171+ShaneEDouglas@users.noreply.github.com>
2025-05-26 10:50:19 -0400

    added server testing code

--- src/main/java/com/example/inventorymangamentsystem/controllers/AuthController.java
+++ src/main/java/com/example/inventorymangamentsystem/controllers/AuthController.java
@@ -1,4 +1,16 @@
 package com.example.inventorymangamentsystem.controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class AuthController {
+
+    @GetMapping("/test")
+    public Map<String, String> test() {
+        return Map.of("Status: ", "Auth Works");
+    }
 }
---
+++ src/main/java/com/example/inventorymangamentsystem/controllers/DefaultRouteController.java
@@ -0,0 +1,21 @@
+package com.example.inventorymangamentsystem.controllers;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class DefaultRouteController {
+
+    @GetMapping("/")
+    public Map<String, String> index(HttpServletRequest request) {
+        String baseUrl = request.getRequestURL().toString();
+        return Map.of("Message:", "Um I think your server is working");
+
+    }
+
+
+}
